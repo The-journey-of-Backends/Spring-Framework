59584b281cb666289cb5cb4460b88db0a011da71
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-07-06 02:14:43 +0900

    :tada: initial commit

---
+++ src/test/java/springframe/Initial_spring/InitialSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package springframe.Initial_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
