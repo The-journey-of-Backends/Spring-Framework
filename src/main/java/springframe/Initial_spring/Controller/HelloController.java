c1b575e28dacd4fa95b1b53d8644dbee9bcac659
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-07-14 00:01:32 +0900

    :hammer: MVC와 템플릿 엔진 코드 수정

--- src/main/java/springframe/Initial_spring/Controller/HelloController.java
+++ src/main/java/springframe/Initial_spring/Controller/HelloController.java
@@ -16,9 +16,10 @@ public class HelloController {
     }
 
     @GetMapping("hello-mvc")
-    @ResponseBody //를 사용하변 뷰 리졸버를 사용하지 않음
-    // 여기서 뷰 리졸버란 helloMVC 객체를 View 영역으로 전달하기 위 알맞은 View의 정보를 설정하는 역할을 한다.
-    public String helloMvc(@RequestParam("name") String name){ //@RequestParam은 Http 요청 파리미터 값을 편리하게 사용하게 해주는 기능입니다.
-        return "hello-mvc" + name;
+    public String helloMvc(@RequestParam("name") String name, Model model){ //@RequestParam은 Http 요청 파리미터 값을 편리하게 사용하게 해주는 기능입니다.
+        model.addAttribute("name",name);
+        return "hello-template";
     }
+
+
 }
