e35114b08d13a040e04ef98ccb28b46bec0640c0
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-07-13 23:36:34 +0900

    :sparkles: 스프링 웹 개발의 기초[MVC와 템플릿 엔진]

--- src/main/java/springframe/Initial_spring/Controller/HelloController.java
+++ src/main/java/springframe/Initial_spring/Controller/HelloController.java
@@ -3,6 +3,8 @@ package springframe.Initial_spring.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,12 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    @ResponseBody //를 사용하변 뷰 리졸버를 사용하지 않음
+    // 여기서 뷰 리졸버란 helloMVC 객체를 View 영역으로 전달하기 위 알맞은 View의 정보를 설정하는 역할을 한다.
+    public String helloMvc(@RequestParam("name") String name){ //@RequestParam은 Http 요청 파리미터 값을 편리하게 사용하게 해주는 기능입니다.
+        return "hello-mvc" + name;
     }
 }
