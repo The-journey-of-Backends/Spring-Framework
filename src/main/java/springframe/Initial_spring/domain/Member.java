d3ee25e5358dc98e85ddf3d67613cad243a32ccd
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-07-17 01:28:13 +0900

    :sparkles: 회원 도메인과 리포지토리 제작

--- src/main/java/springframe/Initial_spring/domain/Member.java
+++ src/main/java/springframe/Initial_spring/domain/Member.java
@@ -1,22 +1,12 @@
 package springframe.Initial_spring.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+// lombok을 사용해서 Getter Setter 적용
+@Getter
+@Setter
 public class Member {
     private Long id;
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
---
+++ src/main/java/springframe/Initial_spring/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package springframe.Initial_spring.repository;
+
+import springframe.Initial_spring.domain.Member;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    // findById or findByName 값이 null일 때 null을 그대로 반환하는 대신 Optional을 통해서 반환한다.
+    Optional<Member> findById(Long id);
+    Optional<Member> findByUsername(String username);
+    List<Member> findAll(); //지금까지 저장된 모든 회원 리스트를 반환해줍니다.
+
+}
