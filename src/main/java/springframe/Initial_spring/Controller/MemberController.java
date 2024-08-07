ed98c2d090d96b73ffc9823d2d680a6e385b85bd
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-08 01:23:59 +0900

    :sparkles: 홈 화면 등록

--- src/main/java/springframe/Initial_spring/Controller/MemberController.java
+++ src/main/java/springframe/Initial_spring/Controller/MemberController.java
@@ -2,6 +2,9 @@ package springframe.Initial_spring.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import springframe.Initial_spring.domain.Member;
 import springframe.Initial_spring.service.MemberService;
 
 @Controller
@@ -12,5 +15,20 @@ public class MemberController {
     @Autowired // 의존 관계 주입(DI)를 할 때 사용하는 어노테이션(Annotation)이며, 의존 객체의 타입에 해당하는 빈(Bean)을 찾아 주입해주는 역할을 합니다.
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm){
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+
+        return "redirect:/ ";
     }
 }
