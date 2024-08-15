20fc09fe8da50df8ae1d1e7ae363ae17b29ebfc8
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-16 03:01:02 +0900

    :white_check_mark: 스프링 회원가입 통합 테스트

---
+++ src/test/java/springframe/Initial_spring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,32 @@
+package springframe.Initial_spring.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import springframe.Initial_spring.domain.Member;
+import springframe.Initial_spring.repository.MemberRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
+    }
+}
