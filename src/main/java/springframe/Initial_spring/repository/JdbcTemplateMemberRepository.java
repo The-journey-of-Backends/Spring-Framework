3405fdac7eb8edabe866fc5d9242efeca50ed2ad
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-17 03:02:46 +0900

    :zap: 기존 코드 lambda 형으로 개선

--- src/main/java/springframe/Initial_spring/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/springframe/Initial_spring/repository/JdbcTemplateMemberRepository.java
@@ -41,14 +41,11 @@ public class JdbcTemplateMemberRepository implements MemberRepository{
     }
 
     private RowMapper<Member> memberRowMapper(){
-        return new RowMapper<Member>(){
-            @Override
-            public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
-                Member member = new Member();
-                member.setId(rs.getLong("id"));
-                member.setName(rs.getString("name"));
-                return member;
-            }
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
         };
     }
 }
