903d5f591a084b490a4e71cbdf1e54e2c9411d19
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-17 02:52:54 +0900

    :sparkles: JdbcTemplate 파일 생성

---
+++ src/main/java/springframe/Initial_spring/repository/JdbcTemplateMemberRepository.java
@@ -0,0 +1,39 @@
+package springframe.Initial_spring.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import springframe.Initial_spring.domain.Member;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcTemplateMemberRepository implements MemberRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateMemberRepository(DataSource dataSource){
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByUsername(String username) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return List.of();
+    }
+}
