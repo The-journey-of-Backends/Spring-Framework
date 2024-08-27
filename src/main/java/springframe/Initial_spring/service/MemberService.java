2bf5cd4dc22dba2d56b82dba1a18e5162a4e2647
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-27 10:46:09 +0900

    :sparkles: 회원 조회 시간 측정 추가

--- src/main/java/springframe/Initial_spring/service/MemberService.java
+++ src/main/java/springframe/Initial_spring/service/MemberService.java
@@ -49,9 +49,17 @@ public class MemberService {
         });
     }
 
-    // 전체 회원 조회
+    /**
+     *전체 회원 조회 */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findMember(Long id) {
