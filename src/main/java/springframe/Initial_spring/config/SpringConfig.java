742ed55fd85b8a27eb1112bcb3e6765c63025a90
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-27 10:18:46 +0900

    :bug: 기존 datasource 및 em 제거

--- src/main/java/springframe/Initial_spring/config/SpringConfig.java
+++ src/main/java/springframe/Initial_spring/config/SpringConfig.java
@@ -15,19 +15,15 @@ public class SpringConfig {
     // DataSource는 데이터베이스 커넥션을 획득할 때 사용하는 객체입니다.
     // 스프링 부트는 데이터베이스 커넥션 정보를 바탕으로 DataSource를 생성하고 스프링 빈으로 만들어둡니다.
     // 그래서 DI를 받을 수 있습니다.
-    private final DataSource dataSource;
     private final MemberRepository memberRepository;
-    private EntityManager em;
 
-    @Autowired 
-    public SpringConfig(DataSource dataSource, EntityManager em, MemberRepository memberRepository) {
-        this.dataSource = dataSource;
-        this.em = em;
+
+    public SpringConfig(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository);
     }
 
